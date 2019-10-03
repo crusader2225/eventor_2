901c4284f434ae4b9a28d5d83944cf7213f0eefd
Shanen <co1627@yahoo.com>
2019-10-03 01:20:53 -0400

    landing page setup

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,16 @@
+class PagesController < ApplicationController
+  
+
+  def about
+  end
+
+  def contact
+  end
+
+  def index
+  end
+
+
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  get 'pages/about'
+  get 'pages/contact'
+  root 'pages#index'
   resources :events
+  resources :pages
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
